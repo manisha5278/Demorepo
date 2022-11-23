9e8679fbfba2363f8cfda688f35c95ec45171cd4
manisha5278 <118874923+manisha5278@users.noreply.github.com>
2022-11-22 23:07:46 -0800

    This is my first commit for HelloWorld.
    Signed-off-by: manisha5278 <118874923+manisha5278@users.noreply.github.com>

---
+++ Java.Demo/src/HelloNepal.java
@@ -0,0 +1,21 @@
+
+public class HelloNepal {
+
+public static void main(String[] args) {
+System.out.println("Hello Nepal!!");
+ 
+        int x = 78, y = 7;
+ 
+        System.out.println("Before Swap");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+ 
+        int temp = x;
+        x = y;
+        y = temp;
+ 
+        System.out.println("After swap");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
